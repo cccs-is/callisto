204018407f73ebc1cf6148c8b23ddde0dcf44620
besedin <oleg.besedin@165gc.onmicrosoft.com>
2020-01-14 13:58:40 -0500

    Cleanup dependencies

--- gallery/views.py
+++ gallery/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
-from django.conf import settings
 from .helpers import find_notebook_by_keywords, get_all_data_sources
 from .helpers import add_notebook_direct
 from .helpers import paginate_items
--- hub/settings.py
+++ hub/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import dj_database_url
 from env_tools import apply_env
 
 apply_env()
