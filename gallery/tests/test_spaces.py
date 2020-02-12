279300f3b1f9a992d355183a0612a56eb4d12549
cccs-ob <oleg.besedin@cyber.gc.ca>
2020-02-12 10:09:45 -0500

    Added test for direct URL access to notebooks and spaces

--- gallery/tests/test_spaces.py
+++ gallery/tests/test_spaces.py
@@ -1,8 +1,6 @@
 from django.test import TestCase, RequestFactory, Client
 from django.conf import settings
-from gallery.models import SharedNotebook, NotebookLike
-from gallery.views_notebook_details import render_notebook
-import arrow
+from gallery.models import SharedNotebook, HubSpace
 from django.contrib.auth import get_user_model
 
 
@@ -22,9 +20,6 @@ class ViewTest(TestCase):
         response = c.get('/space/')
         self.assertContains(response, 'Julian Green', status_code=200)
 
-        # TODO make it more precise?
-
-        text_response = str(response.content)
         self.assertContains(response, "Exchange", status_code=200)
         self.assertContains(response, "WorkFlow", status_code=200)
         self.assertContains(response, "Department A", status_code=200)
@@ -32,3 +27,43 @@ class ViewTest(TestCase):
 
         self.assertNotContains(response, "Department A - Confidential", status_code=200)
         self.assertNotContains(response, "Department B - Confidential", status_code=200)
+
+    def _direct_notebook_access(self, client, notebook_name, should_work):
+        notebook = SharedNotebook.objects.get(notebook_name=notebook_name)
+        response = client.get('/notebook/{}/'.format(notebook.pk))
+        if should_work:
+            self.assertEqual(response.status_code, 200, "Wrong HTTP code for notebook {}".format(notebook_name))
+        else:
+            self.assertRedirects(response, '/', fetch_redirect_response=False)
+
+    def _direct_space_access(self, client, space_name, should_work):
+        space = HubSpace.objects.get(space_name=space_name)
+        response = client.get('/space/{}/'.format(space.pk))
+        if should_work:
+            self.assertEqual(response.status_code, 200, "Wrong HTTP code for space {}".format(space_name))
+        else:
+            self.assertRedirects(response, '/space', fetch_redirect_response=False)
+
+    def test_direct_access(self):
+        c = Client()
+        user_model = get_user_model()
+
+        # User 'hA' - check direct access to notebooks and spaces
+        ha_user = user_model.objects.get(username='ha')
+        c.force_login(ha_user)
+
+        self._direct_notebook_access(c, 'ip_and_us', True)
+        self._direct_notebook_access(c, 'survey_results_B', True)
+        self._direct_notebook_access(c, 'schedule_B', False)
+        self._direct_notebook_access(c, 'gift_exchange', False)
+
+        self._direct_space_access(c, 'Department A', True)
+        self._direct_space_access(c, 'Department B', False)
+        c.logout()
+
+        # User 'it' - check direct access to spaces as admin
+        it_user = user_model.objects.get(username='it')
+        c.force_login(it_user)
+        self._direct_space_access(c, 'Exchange', True)
+        self._direct_space_access(c, 'Department B - Confidential', True)
+        c.logout()
