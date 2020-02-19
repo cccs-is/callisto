3623318873ea89cef81d8277a75231c469132367
cccs-ob <oleg.besedin@cyber.gc.ca>
2020-02-19 16:16:47 -0500

    Preview for XML documents

--- gallery/doc_types/doc_type_xml.py
+++ gallery/doc_types/doc_type_xml.py
@@ -38,7 +38,34 @@ class DocTypeXML(DocTypeBase):
         return response
 
     @classmethod
+    def _convert(cls, elem, level=0):
+        safe_tag = html.escape(elem.tag)
+        safe_text = html.escape(elem.text) if elem.text else None
+        safe_tail = html.escape(elem.tail) if elem.tail else None
+        has_children = (len(elem) > 0)
+
+        indent = ' style="text-indent: {0}px;"'.format(level * 25) if level else ''
+
+        result = '<p ' + indent + '>'
+        result += level * '  '
+        result += '&lt;' + '<span class="xml-tag">' + safe_tag + '</span>'
+        for name, value in elem.attrib.items():
+            result += ' ' + '<span class="xml-attr-name">' + html.escape(name) + '</span>' + '=&quot;' + '<span class="xml-attr-value">' + html.escape(value) + '</span>' + '&quot;'
+        result += '&gt;'
+
+        if safe_text:
+            result += safe_text
+        if has_children:
+            result += "</p>"
+            for child in elem:
+                result += cls._convert(child, level + 1)
+            result += '<p ' + indent + '>'
+        if safe_tail:
+            result += safe_tail
+        result += '&lt;/' + '<span class="xml-tag">' + safe_tag + '</span>' + '&gt;</p>'
+        return result
+
+    @classmethod
     def render(cls, request, document):
-        # TODO do some form of pretty-print
-        body = '<div>' + html.escape(document.document_content) + '</div>'
-        return body
+        root = ElementTree.fromstring(document.document_content)
+        return cls._convert(root)
